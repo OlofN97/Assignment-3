adab5a51fb0edb54d79e40ddbc022e8b3c522e61
Tpbezerra <81422026+Tpbezerra@users.noreply.github.com>
2022-05-09 16:41:01 +0200

    Pool and Spawner
    Added a spawner that places units (that don't do anything yet) in a circle. One can choose whether it should use object-pooling or instantiation.

---
+++ Assignment 3/Assets/_Scripts/ObjectPool.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool<T> where T : MonoBehaviour
+{
+    readonly T objectToPool;
+
+    readonly Transform objectsParent;
+
+    readonly LinkedList<T> pool;
+
+    public T[] GetPoolObjects()
+    {
+        T[] toReturn = new T[pool.Count];
+        pool.CopyTo(toReturn, 0);
+        return toReturn;
+    }
+
+    public ObjectPool(T poolObject, int startingCapacity) : this(poolObject, startingCapacity, null) { }
+
+    public ObjectPool(T poolObject, int startingCapacity, Transform parent)
+    {
+        pool = new LinkedList<T>();
+
+        objectToPool = poolObject;
+        startingCapacity = Mathf.Clamp(startingCapacity, 1, int.MaxValue);
+        objectsParent = parent;
+
+        for (int i = 0; i < startingCapacity; i++)
+        {
+            T temp = Object.Instantiate(objectToPool);
+            temp.transform.SetParent(objectsParent);
+            temp.gameObject.SetActive(false);
+            pool.AddFirst(temp);
+        }
+    }
+
+    public T Pull(Vector3 spawnPosition, Quaternion spawnRotation)
+    {
+        if (pool.First.Value.gameObject.activeInHierarchy)
+            IncreaseSize();
+
+        T spawned = pool.First.Value;
+        pool.RemoveFirst();
+        pool.AddLast(spawned);
+        spawned.transform.SetPositionAndRotation(spawnPosition, spawnRotation);
+        spawned.gameObject.SetActive(true);
+
+        return spawned;
+    }
+
+    public void DisableObject(T toDisable)
+    {
+        toDisable.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+        toDisable.gameObject.SetActive(false);
+        pool.AddFirst(toDisable);
+    }
+
+    void IncreaseSize()
+    {
+        int iterations = pool.Count;
+        for (int i = 0; i < iterations; i++)
+        {
+            T temp = Object.Instantiate(objectToPool);
+            temp.transform.SetParent(objectsParent);
+            temp.gameObject.SetActive(false);
+            pool.AddFirst(temp);
+        }
+    }
+}
---
+++ Assignment 3/Assets/_Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Unit unitToSpawn;
+
+    [SerializeField] bool useObjectPool = true;
+
+    [SerializeField] int poolStartCapacity = 25;
+
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    
+    [SerializeField] Vector2 minMaxSpawnRadius = new Vector2(25, 45);
+
+    public int spawnedUnits;
+
+    float spawnTimer;
+
+    ObjectPool<Unit> unitPool;
+
+    void Start()
+    {
+        unitPool = new ObjectPool<Unit>(unitToSpawn, poolStartCapacity);
+    }
+
+    void Update()
+    {
+        while (spawnTimer >= timeBetweenSpawns && timeBetweenSpawns > 0)
+        {
+            Vector2 circlePosition = Random.onUnitSphere * Random.Range(minMaxSpawnRadius.x, minMaxSpawnRadius.y);
+            Vector3 spawnPosition = new Vector3(circlePosition.x, transform.position.y, circlePosition.y);
+
+            if (useObjectPool)
+                unitPool.Pull(spawnPosition, Quaternion.identity);
+            else
+                Instantiate(unitToSpawn, spawnPosition, Quaternion.identity);
+
+            spawnedUnits++;
+
+            spawnTimer -= timeBetweenSpawns;
+        }
+
+        spawnTimer += Time.deltaTime;
+    }
+}
