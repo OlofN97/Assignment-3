1c504a9bd566e48faca98607ad117cbe318bef00
IronGrahn <irongrahn@gmail.com>
2022-05-11 10:57:30 +0200

    Added a spawner
    Added the spawner which handles enemy spawning through instanciationsnonn (eller hur fan det nu stavas. Kommer ens nån läsa det här? Why are we still here?)

---
+++ Assignment 3/Assets/SpawnerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject enemyType;
+    public List<GameObject> enemies;
+    public GameObject player;
+
+    public float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnEnemy", 2);
+    }
+
+    void SpawnEnemy()
+    {
+        Vector3 pos = Random.insideUnitSphere*25;
+
+        pos.y = 1;
+
+        if(Vector3.Distance(player.transform.position, pos)>distance )
+        {
+            GameObject newEnemy = Instantiate(enemyType, pos, Quaternion.identity);
+            enemies.Add(newEnemy);
+            Invoke("SpawnEnemy", 0.0f);
+        }
+        else
+        {
+            Invoke("SpawnEnemy", 0);
+        }
+
+
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i=0; i<enemies.Count;i++)
+        {
+
+        }
+        
+    }
+}
